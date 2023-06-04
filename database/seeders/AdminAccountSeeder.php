8d07b01805d62eb4eac972029f064b67ace2febc
aqeel_saeed <aqeelraiee210@gmail.com>
2023-06-04 19:32:58 +0300

    Add Admin Account Seeder

---
+++ database/seeders/AdminAccountSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminAccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()->insert([
+            'first_name_en' => 'admin',
+            'last_name_en' => 'admin',
+            'first_name_ar' => 'المسؤول',
+            'last_name_ar' => 'المسؤول',
+//            'encoded_image' => ,
+            'phone_number' => '0999999999',
+            'gender' => 'male',
+            'role' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => 'admin123!',
+        ]);
+    }
+}
